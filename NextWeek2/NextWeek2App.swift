9102b280189a2e52f79a75feaaf6af797f4e362c
Jose Antonio Mendoza <66326890+j0sm3n@users.noreply.github.com>
2025-05-01 17:37:17 +0200

    Initial Commit

---
+++ NextWeek2/NextWeek2App.swift
@@ -0,0 +1,17 @@
+//
+//  NextWeek2App.swift
+//  NextWeek2
+//
+//  Created by Jose Antonio Mendoza on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct NextWeek2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
